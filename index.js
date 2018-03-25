1e4eaac9a19acdb2f523f25a6b215d9060265f3b
John Rodriguez <jcr2125@gmail.com>
2018-03-25 21:23:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ function updateObjectWithKeyAndValue(obj, key, value){
   return Object.assign({}, obj, { [key]: value });
 }
 
-function 
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(obj, {[key]: value})
+}
