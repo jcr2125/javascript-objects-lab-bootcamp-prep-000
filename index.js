783ae87c182401eee660d334ca48081a824c48dc
John Rodriguez <jcr2125@gmail.com>
2018-03-25 21:26:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,9 @@ function updateObjectWithKeyAndValue(obj, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(obj, key, value){
   return Object.assign(obj, { [key]: value});
 }
+
+function deleteFromObjectByKey(obj, key){
+  var newObj = Object.assign({}, obj);
+  delete newObj[key];
+  return newObj;
+}
